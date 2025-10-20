51647321104a357a6c83c10965bfccc4db704eef
burakcanbz <burak.canbaz.web@gmail.com>
2025-10-20 17:09:18 +0300

    change origin

--- api/app.js
+++ api/app.js
@@ -17,7 +17,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors({
-    origin : true,
+    origin : "https://buyzycareer.onrender.com",
     credentials: true
 }))
 app.use(cookieParser());
