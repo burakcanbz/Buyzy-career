a731395cc39eb1b859dac7ee98a3fc6082ddd098
burakcanbz <burak.canbaz.web@gmail.com>
2025-10-20 18:21:15 +0300

    log for cookie setting

--- api/utils/generateToken.js
+++ api/utils/generateToken.js
@@ -5,11 +5,13 @@ exports.generateToken = (res, userId, role) => {
     const token = jwt.sign({ userId, role }, process.env.JWT_SECRET, {
         expiresIn: '30d'
     });
-
+    
+    console.log("BEFORE TOKEN GENERATE", userId, role);
     res.cookie('careerJwt', token, {
         httpOnly: true,
         secure: true,
         sameSite: 'None',
         maxAge: 30 * 24 * 60 * 60 * 1000 // 30 Days
     })
+    console.log("AFTER TOKEN GENERATE", userId, role);
 }
