0052dea1dfe076b256986211a6c9ab601fb963b4
burakcanbz <burak.canbaz.web@gmail.com>
2025-10-20 19:44:07 +0300

    remove logs from genToken

--- api/utils/generateToken.js
+++ api/utils/generateToken.js
@@ -6,12 +6,10 @@ exports.generateToken = (res, userId, role) => {
         expiresIn: '30d'
     });
     
-    console.log("BEFORE TOKEN GENERATE", userId, role);
     res.cookie('careerJwt', token, {
         httpOnly: true,
         secure: true,
         sameSite: 'None',
         maxAge: 30 * 24 * 60 * 60 * 1000 // 30 Days
     })
-    console.log("AFTER TOKEN GENERATE", userId, role);
 }
