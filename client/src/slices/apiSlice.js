ba6c31f8ae49cae6a5a101c72ac7d8503e351580
burakcanbz <burak.canbaz.web@gmail.com>
2025-10-20 16:09:48 +0300

    change baseURL for production as API

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -1,7 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react' ;
-import { BASE_URL } from '../constants';
 
-const baseQuery = fetchBaseQuery({baseUrl: process.env.NODE_ENV === 'production' ? BASE_URL : '',  credentials: 'include'})
+const baseQuery = fetchBaseQuery({baseUrl: process.env.NODE_ENV === 'production' ? 'https://buyzy-career.onrender.com' : '',  credentials: 'include'})
 
 export const apiSlice = createApi({
     baseQuery,
